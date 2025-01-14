04c28b5d5497b52122a8a0f50157541cf70d1b2a
salma <sasaeed092@gmail.com>
2025-01-14 10:30:35 +0100

    Add root route

--- server.js
+++ server.js
@@ -17,6 +17,9 @@ mongoose.connect(process.env.MONGODB_URI)
 
 
 app.use("/api/flowers", require("./src/routes/flowerRoutes"));
+app.get("/", (req, res) => {
+  res.send("Welcome to the Flower Delivery Backend!");
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
